af99c58b783fd29feb2482d65f594408c86130a6
Luan Tomarozzi <lctomarozzi@gmail.com>
2020-05-05 14:47:10 -0300

    Spotcode quase finalizado

---
+++ app/javascript/screens/favorites/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment, useState } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
+
+const FavoriteScreen = () => {
+    return(
+        <Fragment>
+            <div>Favorite Screen</div>
+            <NavbarFooter/>
+        </Fragment>
+    )
+}
+
+export default FavoriteScreen;
---
+++ app/javascript/services/albums.js
@@ -0,0 +1,9 @@
+import Api from './api'
+
+const AlbumService= {
+    index: () => Api.get('/dashboard'),
+    show: (id) => Api.get(`/albums/${id}`)
+}
+
+export default AlbumService;
+ 
